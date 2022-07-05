63669016bdd5d3d078ebc047c8c1379eb160b414
ryan_shen <ryan_shen@gss.com.tw>
2022-07-06 02:17:30 +0800

    [Feature] Support using icons

--- JableDownloader/JableDownloader/AssemblyInfo.cs
+++ JableDownloader/JableDownloader/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+
+[assembly: Xamarin.Forms.ExportFont("Font Awesome 6 Brands-Regular-400.otf", Alias = "FAB")] 
+[assembly: Xamarin.Forms.ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "FAR")] 
+[assembly: Xamarin.Forms.ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "FAS")] 
